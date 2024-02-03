5e57af5f762b56ed4bcaa79cffd58b414dfea16c
Adarsh9930 <adarshmishra3q9@gmail.com>
2024-02-03 23:56:41 +0530

    Adarsh

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "././Components/././Components/About.jsx";
+import Contact from "././Components/././Components/Contact/Contact.jsx"
+import Footer from"././Components/././Components/Footer/Footer.jsx"
+import Header from "././Components/././Components/Header/Header.jsx"
+
+function App() {
+  <>
+  <About/>
+  <Contact/>
+  <Footer/>
+  <Header/>
+  </>
+}
+
+export default App;
---
+++ src/Components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Home.css"
+
+const AboutMe = () => {
+  return (
+    <section className="about">
+      <h2 className='ab'>About Me</h2>
+      <h3>I am software Developer</h3>
+    </section>
+  );
+};
+
+export default AboutMe;
