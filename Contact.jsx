39d2ff6010ef4eaef61b07893a26ce03e1921765
Adarsh Mishra <151769643+Adarsh9930@users.noreply.github.com>
2024-02-04 00:07:25 +0530

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <section id='contact'>
+      <h2>Contact</h2>
+      <h3 className='mobile'>Mobile No: +83XXXXXXX5</h3>
+      <h3 className='email'>Email Id: adr*****@gmail.com</h3>
+      <h3 className='social-media'>Social Media: Twitter, Facebook, LinkedIn</h3>
+    </section>
+  );
+};
+
+export default Contact;
